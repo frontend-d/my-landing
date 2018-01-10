3caceac82b995584f3eea62d7d24405513a06379
iilyaa <vasilenko2999739@gmail.com>
2018-01-10 15:47:34 +0200

    first comit

---
+++ js/typed-script.js
@@ -0,0 +1,12 @@
+
+
+var options = {
+  strings: ["Живая и отзывчивая верстка вашего сайта","Современный Frontend любой сложности","Верстка адаптивного сайта по низкой цене"],
+  typeSpeed: 50,
+  backSpeed: 30,
+  backDelay: 3000,
+  startDelay: 500
+
+}
+
+var typed = new Typed(".element", options);
